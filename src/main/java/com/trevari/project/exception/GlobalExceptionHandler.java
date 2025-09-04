084930400332da502c008c36c18db64cfa8cd511
Jaehyuk-Lee <alex00728@outlook.com>
2025-09-04 12:09:27 +0900

    feat: BadRequestException - 검색 조건문 파싱 에러

---
+++ src/main/java/com/trevari/project/exception/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.trevari.project.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    /**
+     * 요청이 올바르지 않은 경우 400 BadRequest 응답 반환
+     */
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<Map<String, String>> handleBadRequestException(BadRequestException ex) {
+        Map<String, String> errorResponse = new HashMap<>();
+        errorResponse.put("status", "error");
+        errorResponse.put("message", ex.getMessage());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+    }
+}
