0bcfbfda231a50cfc85e5b5509b14decea4d4320
Jaehyuk-Lee <alex00728@outlook.com>
2025-09-06 23:30:15 +0900

    feat: OpenAPI 설정 추가 및 Swagger UI 활성화

---
+++ src/main/java/com/trevari/project/config/OpenApiConfig.java
@@ -0,0 +1,18 @@
+package com.trevari.project.config;
+
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI().info(
+            new Info()
+                .title("Trevari Project API")
+                .version("1.0")
+                .description("트레바리 도서 API 설명"));
+    }
+}
